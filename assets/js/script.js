7908d25ee8143585f2fb8eca2912fdb6f120e12c
Jennyfer Sanchez <JCsanchez@fepco.com.co>
2024-03-07 16:00:59 -0500

    links

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,20 @@ navItems.forEach(item => {
         item.classList.add('active');
     }
 });
+
+
+window.onload = function() {
+  var listItems = document.querySelectorAll('.list-group-item');
+
+  listItems.forEach(function(listItem) {
+    listItem.addEventListener('mouseover', function() {
+      var icon = this.querySelector('i');
+      icon.classList.add('fa-bounce');
+    });
+
+    listItem.addEventListener('mouseout', function() {
+      var icon = this.querySelector('i');
+      icon.classList.remove('fa-bounce');
+    });
+  });
+};
